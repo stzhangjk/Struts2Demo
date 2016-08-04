07943f31c7cebc59caae3ab92118a4b247ed78ae
Grady0000 <stzhangjk@163.com>
2016-08-04 19:14:37 +0800

    修复charactor-encoding

---
+++ CharactorEncoding/src/main/java/action/UserAction.java
@@ -0,0 +1,32 @@
+package action;
+
+import model.User;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class UserAction extends ActionSupport implements ModelDriven<User>{
+
+	private User user = new User();
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("username=" + getUser().getUsername());
+		return super.execute();
+	}
+
+	@Override
+	public User getModel() {
+		// TODO Auto-generated method stub
+		return user;
+	}
+}
