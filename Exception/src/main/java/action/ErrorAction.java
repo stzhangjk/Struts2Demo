a26ee3d0329c06906956bfb322e40c3be91198e7
Grady0000 <stzhangjk@163.com>
2016-08-04 19:37:05 +0800

    修复exception

---
+++ Exception/src/main/java/action/ErrorAction.java
@@ -0,0 +1,11 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ErrorAction extends ActionSupport{
+	
+	
+	public String doerror() throws Exception{
+		throw new Exception("≤‚ ‘“Ï≥£");
+	}
+}
